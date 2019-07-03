2224001adb218cd012faf0334c584c3f2ef7a508
BAANG <chcelestino@gmail.com>
2019-07-03 10:25:58 -0700

    Insert values into seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO doggies (doggy_name)
+VALUES 
+    ("Kuma, the German Shepherd"),
+    ("Atlas, the Pit Bull"),
+    ("Max, the German Shepherd"),
+    ("Mocha, the Border Collie"),
+    ("Duke, the Alaskan Malamute"),
+    ("Gidget, the Taco Bell Dog")
