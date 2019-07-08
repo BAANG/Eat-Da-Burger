55468fe1ee092634de85c64a6a53a51b819e0c86
BAANG <chcelestino@gmail.com>
2019-07-08 09:57:01 -0700

    Typos

--- config/orm.js
+++ config/orm.js
@@ -1,19 +1,53 @@
 var connection = require('./connection.js')
 
+function printQuestionMarks(numVals) {
+    var arr = [];
+
+    for (var i = 0; i < numVals; i++) {
+        arr.push("?");
+    }
+    return arr.toString();
+}
+
 var orm = {
     // TODO: Create ORM routes that will speak to the SQL server to send to the model => controller to be defined.
 
-    selectAll = function() {
+    selectAll: function (table, next) {
+        var queryString = "SELECT * FROM " + table + ";";
+        console.log(queryString)
+        connection.query(queryString,
+            function (err, res) {
+                if (err) {
+                    throw err;
+                }
+                next(res);
+            })
 
     },
 
-    insertOne = function() {
+    insertOne: function (table, col, val, next) {
+        var queryString = "INSERT INTO " + table;
+        queryString += " (";
+        queryString += col.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(val.length);
+        queryString += ") ";
 
+        console.log(queryString)
+
+        connection.query(queryString, val, function (err, result) {
+            if (err) {
+                throw err;
+            }
+
+            next(result);
+        })
     },
-
-    updateOne = function() {
+    updateOne: function (table, col) {
 
     }
+
 }
 
 module.exports = orm;
