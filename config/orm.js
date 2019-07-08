743e475d8a5f4f761c7f29c30fb6a41a4f1414e8
BAANG <chcelestino@gmail.com>
2019-07-08 10:03:40 -0700

    Complete orm functions

--- config/orm.js
+++ config/orm.js
@@ -9,6 +9,29 @@ function printQuestionMarks(numVals) {
     return arr.toString();
 }
 
+// Helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+    var arr = [];
+  
+    // loop through the keys and push the key/value as a string int arr
+    for (var key in ob) {
+      var value = ob[key];
+      // check to skip hidden properties
+      if (Object.hasOwnProperty.call(ob, key)) {
+        // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+        if (typeof value === "string" && value.indexOf(" ") >= 0) {
+          value = "'" + value + "'";
+        }
+        // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+        // e.g. {sleepy: true} => ["sleepy=true"]
+        arr.push(key + "=" + value);
+      }
+    }
+  
+    // translate array of strings to a single comma-separated string
+    return arr.toString();
+  }
+
 var orm = {
     // TODO: Create ORM routes that will speak to the SQL server to send to the model => controller to be defined.
 
@@ -44,7 +67,24 @@ var orm = {
             next(result);
         })
     },
-    updateOne: function (table, col) {
+    updateOne: function (table, col, condition, next) {
+        var queryString = "UPDATE" + table;
+        queryString += " SET ";
+        queryString += objToSql(col);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+        connection.query(
+            queryString,
+            function (err, result) {
+                if(err) {
+                    throw err;
+                }
+
+                next(result);
+            }
+        )
 
     }
 
