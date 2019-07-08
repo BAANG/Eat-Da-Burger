9e6726adc998ef3f7312f846fac625a387b0cfe2
BAANG <chcelestino@gmail.com>
2019-07-08 12:00:33 -0700

    Create page headers for main.handlebars; complete model

---
+++ models/doggy.js
@@ -0,0 +1,19 @@
+var orm = require("../config/orm.js");
+
+var dog = {
+    all: function(next) {
+        orm.selectAll("doggies", function(res) {
+            next(res);
+        })
+    },
+    create: function(col, val, next) {
+        orm.insertOne("doggies", col, val, function(res) {
+            next(res);
+        }) 
+    },
+    update: function(cols, condition, next) {
+        orm.updateOne("doggies", cols, condition, function(res) {
+            next(res);
+        })
+    }
+}
