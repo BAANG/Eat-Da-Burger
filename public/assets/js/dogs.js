e0d3d42bffc7b6b9b788d91163877a0f5503c3db
BAANG <chcelestino@gmail.com>
2019-07-08 23:53:38 -0700

    Add basic validation for user input

--- public/assets/js/dogs.js
+++ public/assets/js/dogs.js
@@ -1,5 +1,5 @@
-$(function(){
-    $(".change-status").on("click", function(event) {
+$(function () {
+    $(".change-status").on("click", function (event) {
         var id = $(this).data("id");
         var newIsGood = $(this).data("newisgood");
         console.log(newIsGood, " is newIsGood")
@@ -12,7 +12,7 @@ $(function(){
             type: "PUT",
             data: updateStatus
         }).then(
-            function() {
+            function () {
                 console.log("changed isGoodBoy to", newIsGood);
 
                 location.reload();
@@ -21,8 +21,9 @@ $(function(){
     })
 });
 
-$(".create-form").on("submit", function(event) {
+$(".create-form").on("submit", function (event) {
     event.preventDefault();
+
 
     var newDog = {
         name: $("#dog").val().trim(),
@@ -30,18 +31,23 @@ $(".create-form").on("submit", function(event) {
         isGoodBoy: $("[name=isGoodBoy]:checked").val().trim()
     };
 
-    $.ajax("/api/dogs/", {
-        type: "POST",
-        data: newDog
-    }).then(
-        function() {
-            console.log("Created a new dog");
-            location.reload();
-        }
-    )
+    if (newDog.name.length > 0 && newDog.type.length > 0) {
+        $.ajax("/api/dogs/", {
+            type: "POST",
+            data: newDog
+        }).then(
+            function () {
+                console.log("Created a new dog");
+                location.reload();
+            }
+        )
+    } else {
+        alert("Please enter a valid name and type for your dog.")
+    }
+
 })
 
-$(document).on("click", ".delete-dog", function() {
+$(document).on("click", ".delete-dog", function () {
     var id = $(this).data("id")
 
     var queryUrl = "/api/dogs/" + id;
@@ -53,7 +59,7 @@ $(document).on("click", ".delete-dog", function() {
             type: "DELETE",
         }
     ).then(
-        function() {
+        function () {
             console.log("deleted a dog");
             location.reload();
         }
